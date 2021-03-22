ae38597bd40c373d700a164c5e063a60b9abd4b7
Naman <snaman87@gmail.com>
2021-03-22 21:36:39 +0530

    Login and Logout Functionality completed

--- blog_application/blog_application/settings.py
+++ blog_application/blog_application/settings.py
@@ -115,5 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
+LOGIN_REDIRECT_URL = "blog-home"
+LOGIN_URL = "login"
