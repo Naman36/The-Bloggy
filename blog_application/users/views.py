5f309eb2ad4872bfc7727c96acdbc32a76a568cc
Naman <snaman87@gmail.com>
2021-03-23 11:21:54 +0530

    Login Fixed

--- blog_application/users/views.py
+++ blog_application/users/views.py
@@ -4,19 +4,23 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import UserRegisterForm
 
+
 def register(request):
     if request.method == "POST":
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get("username")
-            messages.success(request, f"Your account has been created! You can now login")
-            return redirect("Login")
+            messages.success(
+                request, f"Your account has been created! You can now login"
+            )
+            return redirect("login")
     else:
         form = UserRegisterForm()
     return render(request, "users/register.html", {"form": form})
 
+
 @login_required
 def profile(request):
 
-    return render(request, 'users/profile.html')
+    return render(request, "users/profile.html")
