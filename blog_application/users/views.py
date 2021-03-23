ff2bfda6fc902ba17bd8db2665a5ec04bb8d08c7
Naman <snaman87@gmail.com>
2021-03-23 11:28:03 +0530

    User profile and Profile Image

--- blog_application/users/views.py
+++ blog_application/users/views.py
@@ -14,7 +14,7 @@ def register(request):
             messages.success(
                 request, f"Your account has been created! You can now login"
             )
-            return redirect("Login")
+            return redirect("login")
     else:
         form = UserRegisterForm()
     return render(request, "users/register.html", {"form": form})
